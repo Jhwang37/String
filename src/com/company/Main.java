d60f6e6e816d45028d95283bd2ebed7174af3b06
jhwang37 <jhwang37@gmail.com>
2020-01-23 11:22:19 -0500

    first commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	String myName;
+	myName = "James Hwang";
+        System.out.println(myName);
+    }
+}
